3a9b944be8176d9e4540e09b07048cc79bb316dc
ladimiroING <ladimiro1010yahoo.com>
2022-11-04 11:14:54 -0500

    Implementado tabla de comentarios en el componente de listar comentarios

---
+++ src/app/components/listar-comentario/listar-comentario.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Comentario } from 'src/app/interfaces/comentario';
+
+@Component({
+  selector: 'app-listar-comentario',
+  templateUrl: './listar-comentario.component.html',
+  styleUrls: ['./listar-comentario.component.css']
+})
+export class ListarComentarioComponent implements OnInit {
+
+  listarComentarios: Comentario[]=[
+    // {titulo:"Java", 
+    // creador:"James", 
+    // fechaCreacion:new Date(), 
+    // texto:"Orientado a objetos"
+    // },
+    // {titulo:"C#", 
+    // creador:"Anders", 
+    // fechaCreacion:new Date(), 
+    // texto:"Orientado a objetos"
+    // },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  img="https://miro.medium.com/max/622/0*iKwwEO0btKL3brZn"
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
